3e25438345676bef7e38056d3fa9560074781308
inaralmeida <inara.ralmeida@gmail.com>
2022-06-21 03:30:38 -0300

    using ScrollView

--- myApp/App.js
+++ myApp/App.js
@@ -1,53 +1,48 @@
 import React, {useState} from 'react';
-import {View, Text, Image, Button, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 const App = () => {
-  const [text, setText] = useState('');
-  const [button, setButton] = useState('');
-
-  function verNome() {
-    text.length === 0 ? alert('Digite seu nome') : setButton(text);
-  }
-
   return (
-    <View style={S.container}>
-      <TextInput
-        style={S.input}
-        placeholder="Digite seu nome"
-        onChangeText={text => setText(text)}
-      />
-      <Button title="Ver texto" onPress={verNome} style={S.button} />
-      <Text style={S.text}>{button}</Text>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+        <View style={styles.box3}></View>
+      </ScrollView>
     </View>
   );
 };
 
 export default App;
 
-const S = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#555555',
   },
-  input: {
-    height: 50,
-    borderWidth: 1,
-    borderColor: '#222',
-    margin: 20,
-    paddingLeft: 5,
-    fontSize: 20,
-    borderRadius: 10,
-    backgroundColor: '#ededed',
-    color: '#555',
+  box1: {
+    height: 250,
+    backgroundColor: 'red',
   },
-
-  text: {
-    textAlign: 'center',
-    fontSize: 50,
-    color: '#f2f2f2',
+  box2: {
+    height: 250,
+    backgroundColor: 'blue',
   },
-  button: {
-    margin: 20,
-    lineHeight: 30,
+  box3: {
+    height: 250,
+    backgroundColor: 'yellow',
+  },
+  box4: {
+    height: 250,
+    backgroundColor: 'green',
   },
 });
