7255cd990eb2c39fb384f4066b6f59971a118031
inaralmeida <inara.ralmeida@gmail.com>
2022-06-21 04:03:54 -0300

    using FlatLits

--- myApp/App.js
+++ myApp/App.js
@@ -7,18 +7,25 @@ import {
   StyleSheet,
   TextInput,
   ScrollView,
+  FlatList,
 } from 'react-native';
 
 const App = () => {
+  const [feed, setFeed] = useState([
+    {id: 0, name: 'Inara', idade: 23, email: 'inara@gmail.com'},
+    {id: 1, name: 'Carol', idade: 23, email: 'carol@gmail.com'},
+    {id: 2, name: 'Dino', idade: 23, email: 'dino@gmail.com'},
+    {id: 3, name: 'José', idade: 23, email: 'jose@gmail.com'},
+    {id: 4, name: 'Marcel', idade: 23, email: 'marcel@gmail.com'},
+    {id: 5, name: 'Maria', idade: 23, email: 'maria@gmail.com'},
+  ]);
   return (
     <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.box1}></View>
-        <View style={styles.box2}></View>
-        <View style={styles.box3}></View>
-        <View style={styles.box4}></View>
-        <View style={styles.box3}></View>
-      </ScrollView>
+      <FlatList
+        data={feed}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <Card data={item} />}
+      />
     </View>
   );
 };
@@ -29,20 +36,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  box1: {
-    height: 250,
-    backgroundColor: 'red',
+  card: {
+    height: 150,
+    backgroundColor: '#555',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
   },
-  box2: {
-    height: 250,
-    backgroundColor: 'blue',
-  },
-  box3: {
-    height: 250,
-    backgroundColor: 'yellow',
-  },
-  box4: {
-    height: 250,
-    backgroundColor: 'green',
+  infos: {
+    color: 'white',
   },
 });
+
+const Card = ({data}) => {
+  const {name, idade, email} = data;
+  return (
+    <View style={styles.card}>
+      <Text style={styles.infos}>{name}</Text>
+      <Text style={styles.infos}>{idade}</Text>
+      <Text style={styles.infos}>{email}</Text>
+    </View>
+  );
+};
