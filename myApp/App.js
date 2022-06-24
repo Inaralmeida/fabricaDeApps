2a1c662afa826d8d81b6bc95dfb26a3371afdcce
inaralmeida <inara.ralmeida@gmail.com>
2022-06-24 00:16:40 -0300

    fix page home

--- myApp/App.js
+++ myApp/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Home from './src/Pages/Home/Home';
 import Registration from './src/Pages/Registration/Registration';
 import Start from './src/Pages/Start/Start';
 
 const App = () => {
+  const [page, setPage] = useState('home');
+  const isHome = page === 'home';
+  const isRegistration = page === 'registration';
+  const isStart = page === 'start';
+
+  function nextPage(page) {
+    setPage(page);
+  }
   return (
     <View style={styles.container}>
-      <Home />
-      <Registration />
-      <Start />
+      {isHome && <Home nextPage={() => nextPage('registration')} />}
+      {isRegistration && (
+        <Registration nextPage={() => nextPage('registration')} />
+      )}
+      {isStart && <Start nextPage={() => nextPage('start')} />}
     </View>
   );
 };
@@ -19,5 +29,6 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#101010',
   },
 });
--- myApp/src/Pages/Registration/Registration.js
+++ myApp/src/Pages/Registration/Registration.js
@@ -1,9 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 
 const Registration = () => {
   return (
     <View>
+      <Text>Registration</Text>
+      <Text>Registration</Text>
       <Text>Registration</Text>
     </View>
   );
