7bb3ca63b756e988d8a43aaaba9d455723e5a355
inaralmeida <inara.ralmeida@gmail.com>
2022-06-28 22:13:33 -0300

    using icons

--- myApp/App.js
+++ myApp/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import ModalComponent from './src/Components/ModalComponent/ModalComponent';
+import VectorIcons from './src/Components/VectorIcons/VectorIcons';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>Hello word</Text>
+      <VectorIcons />
     </View>
   );
 };
---
+++ myApp/src/Components/VectorIcons/VectorIcons.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Feathers from 'react-native-vector-icons/Feather';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const VectorIcons = () => {
+  return (
+    <View>
+      <FontAwesome5 name="house-user" size={55} color="#ffb200" />
+    </View>
+  );
+};
+
+export default VectorIcons;
